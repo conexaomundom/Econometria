9318da08cb6b31fb33af8cca3b32957972c44bab
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-03-28 22:02:42 -0300

    Rodando já dois modelos de regressão linear.

--- Segunda_Prova_econometria.R
+++ Segunda_Prova_econometria.R
@@ -21,7 +21,7 @@ plot(banco[,-1], main = "Diagrama de dispersão de todas as variéveisdo banco: 
 #b) Regressão bivariada do paladar contra o ácido acético e H2S e interprete os resultados obtidos.
 
 plot(Paladar ~ H2_S, xlab = "Paladar", ylab ="H2S", main = "Diagrama de dispersão entre Paladar e H2S") 
-plot(Paladar ~ Acido_Acetico,xlab = "Paladar", ylab ="Ácido Acético", main = "Diagrama de dispersão entre Paladar e H2S")
+plot(Paladar ~ Acido_Acetico,xlab = "Paladar", ylab ="Ácido Acético", main = "Diagrama de dispersão entre Paladar e Ácido Acético")
 plot(H2_S ~ Acido_Acetico, xlab ="H2S", ylab = "Ácido Acético", main = "Diagrama de dispersão entre H2S e Ácido Acético")
 
 
@@ -38,8 +38,59 @@ plot(H2_S ~ Acido_Acetico, xlab ="H2S", ylab = "Ácido Acético", main = "Diagra
 # se trata de de duas variáveis explicativas agora esse resultado é interessante para nós
 # porque como não tem uma relação linear não tão clara, pode ser um indício que pode não haver
 # colinearidade entre as variáveis.
-cor(banco[,-1])
+xtable(cor(banco[,c(2,3,4)]))
 ## Além de todas essas visualizações da relação das variávies ao calcular a matriz de correlação
 ## a correlação entre a variável dependente Paladar e a variável explicativa H2S é de 0.75575227,
 ## uma correlação boa o que dá indicios de um possível modelo de regressão linear bom, já a 
-## 
+## Já a correlação entre 0.2953833, uma correlação baixa, confirmando o que foi visto no gráfico,
+## será que essa variável será explicativa para o modelo de regressão linear, bem depende, 
+## por isso vamos rodar o modelo e ver se será siginificativa ou não para o modelo. E a correlação
+## entre as duas variáveis explicativas H2S e Ácido Acético foi pequena de 0.1843119 e é o que desejamos
+## porém a relação delas pode não ser linear.
+
+modelo_linear <- lm(Paladar ~ Acido_Acetico + H2_S)
+summary(modelo_linear)
+# Como o esperado ao pegar o summary do modelo linear de regressão com duas variáveis explicativas sendo
+# elas Acido_Acetico e H2_S para explicar a variável Paladar, a variável Ácido Acético será retirada
+# do modelo pois não passou no teste t não rejeitando a hipotese de nao ser significativo para o modelo
+# com p-valor de 0.205, enquanto o a outra variável explicativa passou pelo teste t demonstrando ser significativ
+# para o modelo, bem após retirar essa variável vamos olhar o resultado do modelo.
+
+modelo_linear1 <- lm(Paladar ~  H2_S)
+summary(modelo_linear1)
+
+# Rodando o modelo apenas com uma variável explicativa, sendo ela H2S o intercepto não passa pelo teste t
+# com p-valor de 0.112. Vamos rodar novamente o modelo e verficar o resultado.
+
+modelo_linear3 <- lm(Paladar ~ 0 + H2_S)
+summary(modelo_linear3)
+# Então o modelo final ficou apenas H2S explicando o Paladar, com um poder explicativo de 85.99% da
+# variabilidade do Paladar é explicado por H2S, sendo um bom poder explicativo.
+
+# c) ###########################################
+# regressão bivariada do paladar contra o ácido lático e H2S e interprete os resultados obtidos. 
+plot(Paladar ~ H2_S, xlab = "Paladar", ylab ="H2S", main = "Diagrama de dispersão entre Paladar e H2S") 
+plot(Paladar ~ Acido_Latico,xlab = "Paladar", ylab ="Ácido Lático", main = "Diagrama de dispersão entre Paladar e Ácido Lático")
+plot(H2_S ~ Acido_Latico, xlab ="H2S", ylab = "Ácido Lático", main = "Diagrama de dispersão entre H2S e Ácido Lático")
+xtable(cor(banco[,c(2,4,5)]))
+
+# O gráfico de dispersão entre Paladar e H2S ja foi analisado anteriormente.
+# O gráfico de dispersão entre Paladar e Ácido Lático demonstra uma certa relação
+# linear sim e com uma correlação de 0.7042362, sendo boa para a realização do
+# modelo de regressão linear. Já o gráfico de dispersão entre as duas variáveis
+# explicativas pode estar demonstrando certa relação, o que não é muito bom, além de 
+# ter uma correlação de 0.6448123 que poe ser indicio de colinearidade.
+
+modelo_linear0.1 <- lm(Paladar ~ H2_S + Acido_Latico)
+summary(modelo_linear0.1)
+# O modelo de regressão linear final ficou com as duas variáveis explicativas
+# mais o intercep, todos demonstrando ser significantes para o modelo porém
+# o R2 não foi satisfatorio demonstrando que cerca de 62.59% da variabilidade
+# do Paladar é explicado por H2S e Ácido Lático.
+
+
+
+
+
+
+
